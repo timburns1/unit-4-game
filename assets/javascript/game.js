4acbbb7d56f2c52b70a585c75e9aa244bf89c282
Tim Burns <burnstimothy87@gmail.com>
2019-10-13 15:01:26 -0400

    footer problem

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
-    const random=Math.floor(Math.random()*101+19)
+    let random=Math.floor(Math.random()*101+19)
 
-$('compNumber').text(random);
+$('#compNumber').text(random);
 
 let num1= Math.floor(Math.random()*11+1)
 let num2= Math.floor(Math.random()*11+1)
@@ -13,18 +13,18 @@ let wins= 0;
 let losses=0;
 
 $('#numberWin').text(wins);
-$('numberLost').text(losses);
+$('#numberLost').text(losses);
 
 function reset(){
     random=Math.floor(Math.random()*101+19);
     console.log(random)
     $('#compNumber').text(random);
-    num1= Math.flood(Math.random()*11+1);
-    num2= Math.flood(Math.random()*11+1);
-    num3= Math.flood(Math.random()*11+1);
-    num4= Math.flood(Math.random()*11+1);
-    userTotal=0;
-    $('#finalScore'.text(userScore));
+    num1= Math.floor(Math.random()*11+1);
+    num2= Math.floor(Math.random()*11+1);
+    num3= Math.floor(Math.random()*11+1);
+    num4= Math.floor(Math.random()*11+1);
+    userScore= 0;
+    $('#finalScore').text(userScore);
 };
 
 function winner(){
@@ -32,16 +32,16 @@ function winner(){
         wins++;
         $('#numberWin').text(wins);
         reset();
-}
+};
 
 function loser(){
     alert("You Lost!");
     losses++;
     $('#numberLost').text(losses);
     reset();
-}
+};
 
-$('gemOne').on('click', function(){
+$('#gemOne').on('click', function(){
     userScore = userScore + num1;
     console.log("New userScore=" + userScore);
     $('#finalScore').text(userScore);
@@ -52,8 +52,46 @@ $('gemOne').on('click', function(){
     else if (userScore > random){
         loser();
     }
-})
+});
 
+$('#gemTwo').on('click', function(){
+    userScore = userScore + num2;
+    console.log("New userScore=" + userScore);
+    $('#finalScore').text(userScore);
+
+    if (userScore == random){
+        winner();
+    }
+    else if (userScore > random){
+        loser();
+    }
+});
+
+$('#gemThree').on('click', function(){
+    userScore = userScore + num3;
+    console.log("New userScore=" + userScore);
+    $('#finalScore').text(userScore);
+
+    if (userScore == random){
+        winner();
+    }
+    else if (userScore > random){
+        loser();
+    }
+});
+
+$('#gemFour').on('click', function(){
+    userScore = userScore + num4;
+    console.log("New userScore=" + userScore);
+    $('#finalScore').text(userScore);
+
+    if (userScore == random){
+        winner();
+    }
+    else if (userScore > random){
+        loser();
+    }
+});
 
 
 
